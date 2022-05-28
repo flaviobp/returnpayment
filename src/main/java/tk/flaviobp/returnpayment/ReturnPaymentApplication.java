368fbee323b7728225e690f1b1ea7d7c30f97870
flaviobp@gmail.com <flaviobp@gmail.com>
2022-05-28 11:04:11 -0300

    Initial commit

---
+++ src/main/java/tk/flaviobp/returnpayment/ReturnPaymentApplication.java
@@ -0,0 +1,13 @@
+package tk.flaviobp.returnpayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReturnPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReturnPaymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tk/flaviobp/returnpayment/ReturnPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package tk.flaviobp.returnpayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReturnPaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
